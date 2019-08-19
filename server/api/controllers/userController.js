036a8bdc75dfe1443a75eadbdffa7037568e113c
mahditalebi76 <atrabiboy@gmail.com>
2019-08-19 12:18:12 +0430

    deleteuser3

--- server/api/controllers/userController.js
+++ server/api/controllers/userController.js
@@ -1,10 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-<<<<<<< HEAD
-const {
-	validationResult
-} = require('express-validator');
+const { validationResult } = require('express-validator');
 const randomstring = require('randomstring');
 const ProjectMembers = require('../models/projectMembers');
 const Projects = require('../models/projects');
@@ -12,41 +9,21 @@ const Users = require('../models/users');
 const errorHandler = require('./errorHandler');
 const NotConfirmedUsers = require('../models/notConfirmedUsers');
 const mail = require('../controllers/mailController');
-=======
-const jwtSecret = config.get('app.webServer.jwtSecret');
-const {
-	validationResult
-} = require('express-validator');
-const ProjectMembers = require('../models/projectMembers');
-const Project = require('../models/project');
->>>>>>> meti
 
 const jwtSecret = config.get('app.webServer.jwtSecret');
 // ! FOR TEST ONLY
 exports.getUserInfo = (req, res) => {
-<<<<<<< HEAD
 	Users.findAll({
-=======
-	User.findAll({
->>>>>>> meti
-			where: {
-				userId: req.user.userId
-			}
-		})
-<<<<<<< HEAD
+		where: {
+			userId: req.user.userId
+		}
+	})
 		.then((user) => {
-=======
-		.then(user => {
->>>>>>> meti
 			return res.status(200).json({
 				userInfo: user[0]
 			});
 		})
-<<<<<<< HEAD
 		.catch((err) => {
-=======
-		.catch(err => {
->>>>>>> meti
 			return res.status(500).json({
 				err
 			});
@@ -55,27 +32,23 @@ exports.getUserInfo = (req, res) => {
 
 exports.getUserProjects = (req, res) => {
 	ProjectMembers.findAll({
-			where: {
-				memberId: req.user.userId
-			},
-			include: [{
-<<<<<<< HEAD
+		where: {
+			memberId: req.user.userId
+		},
+		include: [
+			{
 				model: Projects,
 				attributes: ['title', 'projectId', 'description', 'createdAt'],
-				include: [{
-					model: Users,
-=======
-				model: Project,
-				attributes: ['title', 'projectId', 'description', 'createdAt'],
-				include: [{
-					model: User,
->>>>>>> meti
-					as: 'creator',
-					attributes: ['name']
-				}]
-			}]
-		})
-<<<<<<< HEAD
+				include: [
+					{
+						model: Users,
+						as: 'creator',
+						attributes: ['name']
+					}
+				]
+			}
+		]
+	})
 		.then((projects) => {
 			return res.status(200).json(
 				projects.map((project) => {
@@ -84,12 +57,6 @@ exports.getUserProjects = (req, res) => {
 			);
 		})
 		.catch((err) => {
-=======
-		.then(projects => {
-			return res.status(200).json(projects.map(project => project.project));
-		})
-		.catch(err => {
->>>>>>> meti
 			return res.status(500).json({
 				err
 			});
@@ -97,31 +64,20 @@ exports.getUserProjects = (req, res) => {
 };
 
 exports.signUp = (req, res) => {
-<<<<<<< HEAD
 	// TODO FIX ERRORHANDLER
 	// first we go to validation to validate the errors.
 	// then we get a list and we call errorHandler.handler on it
 	// then we get another list called handledErrorsList which contains errors
-=======
-	//TODO FIX ERRORHANDLER
-	//first we go to validation to validate the errors.  then we get a list and we call errorHandler.handler on it
-	//then we get another list called handledErrorsList which contains errors beautifully
->>>>>>> meti
 	const errorsList = validationResult(req).errors;
 	const handledErrorsList = errorHandler.handler(errorsList);
 	if (Object.keys(handledErrorsList).length > 0) {
 		return res.status(422).json(handledErrorsList);
 	}
-<<<<<<< HEAD
 	// hasing the password
-=======
-	//hasing the password
->>>>>>> meti
 	bcrypt.hash(req.body.password, 10, (err, hash) => {
 		if (err) {
 			return res.status(500).json({
 				message: 'sign up failed',
-<<<<<<< HEAD
 				error: 'cannot hash',
 				err
 			});
@@ -135,12 +91,12 @@ exports.signUp = (req, res) => {
 				.emailVerification(req.body.email, confirmationCode)
 				.then((response) => {
 					NotConfirmedUsers.create({
-							userName: req.body.userName,
-							email: req.body.email,
-							name: req.body.name,
-							password: hash,
-							confirmationCode
-						})
+						userName: req.body.userName,
+						email: req.body.email,
+						name: req.body.name,
+						password: hash,
+						confirmationCode
+					})
 
 						// sign up success
 						.then((user) => {
@@ -185,47 +141,17 @@ exports.signUp = (req, res) => {
 			// 	});
 
 			// create user in database with the given attributes
-=======
-				err: 'cannot hash',
-				err
-			});
-		} else if (hash) {
-			//create user in database with the given attributes
-			User.create({
-					userName: req.body.userName,
-					email: req.body.email,
-					name: req.body.name,
-					password: hash
-				})
-				//sign up success
-				.then(user => {
-					return res.status(200).json({
-						message: 'sign up complete',
-						userName: user.userName,
-						email: user.email,
-						userId: user.userId
-					});
-				})
-				//sign up failed
-				.catch(err => {
-					return res.status(500).json({
-						message: 'sign up failed',
-						err
-					});
-				});
->>>>>>> meti
 		}
 	});
 };
 
 exports.signIn = (req, res) => {
-<<<<<<< HEAD
 	// finding the user in database with given email
 	NotConfirmedUsers.findAll({
-			where: {
-				email: req.body.email
-			}
-		})
+		where: {
+			email: req.body.email
+		}
+	})
 		.then((unconfirmedUser) => {
 			console.log(unconfirmedUser);
 			if (unconfirmedUser.length != 0) {
@@ -238,10 +164,10 @@ exports.signIn = (req, res) => {
 		.catch();
 
 	Users.findAll({
-			where: {
-				email: req.body.email
-			}
-		})
+		where: {
+			email: req.body.email
+		}
+	})
 		.then((user) => {
 			// if user with such email does not exist
 			if (user.length == 0) {
@@ -271,7 +197,8 @@ exports.signIn = (req, res) => {
 							};
 							jwt.sign(
 								jwtpayload,
-								jwtSecret, {
+								jwtSecret,
+								{
 									expiresIn: '10h'
 								},
 								(err, encoded) => {
@@ -301,10 +228,10 @@ exports.signIn = (req, res) => {
 
 module.exports.confirmEmail = (req, res) => {
 	NotConfirmedUsers.findAll({
-			where: {
-				email: req.body.email
-			}
-		})
+		where: {
+			email: req.body.email
+		}
+	})
 		.then((users) => {
 			let confirmedUser = users[0];
 			if (req.body.code == confirmedUser.confirmationCode) {
@@ -341,13 +268,16 @@ module.exports.forgotPassword = (req, res) => {
 			mail
 				.forgotPassword(users[0].email, forgotPasswordCode)
 				.then(() => {
-					Users.update({
-						forgotPasswordCode
-					}, {
-						where: {
-							email: users[0].email
+					Users.update(
+						{
+							forgotPasswordCode
+						},
+						{
+							where: {
+								email: users[0].email
+							}
 						}
-					});
+					);
 					return res.status(200).json({
 						message: 'forgot password message sent'
 					});
@@ -373,13 +303,16 @@ module.exports.forgotPassword = (req, res) => {
 			mail
 				.forgotPassword(users[0].email, forgotPasswordCode)
 				.then(() => {
-					NotConfirmedUsers.update({
-						forgotPasswordCode
-					}, {
-						where: {
-							email: users[0].email
+					NotConfirmedUsers.update(
+						{
+							forgotPasswordCode
+						},
+						{
+							where: {
+								email: users[0].email
+							}
 						}
-					});
+					);
 					return res.status(200).json({
 						message: 'forgot password message sent'
 					});
@@ -402,13 +335,16 @@ module.exports.changePassword = (req, res) => {
 			bcrypt.hash(req.body.password, 10, (error, hash) => {
 				if (hash) {
 					if (req.body.forgotPasswordCode == users[0].forgotPasswordCode) {
-						Users.update({
-							password: hash
-						}, {
-							where: {
-								email: users[0].email
+						Users.update(
+							{
+								password: hash
+							},
+							{
+								where: {
+									email: users[0].email
+								}
 							}
-						}).then(() => {
+						).then(() => {
 							return res.status(200).json({
 								message: 'password changed1'
 							});
@@ -416,64 +352,12 @@ module.exports.changePassword = (req, res) => {
 					} else if (error) {
 						return res.status(500).json({
 							message: 'hashing failure !'
-=======
-	//finding the user in database with given email
-	User.findAll({
-		where: {
-			email: req.body.email
-		}
-	}).then(user => {
-		// if user with such email does not exist
-		if (user.length == 0) {
-			return res.status(404).json({
-				message: 'email or password incorrect'
-			});
-			// if user with this email exists
-		} else {
-			let firstuser = user[0];
-			//checking if the password given is equal to the password in database
-			bcrypt.compare(req.body.password, firstuser.password, (err, check) => {
-				if (err) {
-					return res.status(500).json({
-						message: 'compare not complete'
-					});
-				} else {
-					// if passwords are correct
-					if (check) {
-						//creating the jwt for user
-						const jwtpayload = {
-							name: firstuser.name,
-							email: firstuser.email,
-							userId: firstuser.userId,
-							userName: firstuser.userName
-						};
-						jwt.sign(
-							jwtpayload,
-							jwtSecret, {
-								expiresIn: '10h'
-							},
-							(err, encoded) => {
-								if (err) {
-									throw new Error('error in jwt');
-								}
-								return res.status(200).json({
-									done: true,
-									secret: 'bearer ' + encoded
-								});
-							}
-						);
-						//if passwords did not match that means password incorrect
-					} else {
-						return res.status(404).json({
-							message: 'email or password incorrect'
->>>>>>> meti
 						});
 					}
 				}
 			});
 		}
 	});
-<<<<<<< HEAD
 
 	NotConfirmedUsers.findAll({
 		where: {
@@ -484,7 +368,8 @@ module.exports.changePassword = (req, res) => {
 			bcrypt.hash(req.body.password, 10, (err, hash) => {
 				if (hash) {
 					if (req.body.forgotPasswordCode == users[0].forgotPasswordCode) {
-						NotConfirmedUsers.update({
+						NotConfirmedUsers.update(
+							{
 								password: hash
 							},
 
@@ -506,22 +391,22 @@ module.exports.changePassword = (req, res) => {
 				}
 			});
 		}
-=======
+	});
 };
-
 module.exports.deleteUser = (req, res) => {
 	User.destroy({
 		where: {
 			userId: req.user.userId
 		}
-	}).then(() => {
-		res.status(200).json({
-			message: "User Deleted"
+	})
+		.then(() => {
+			res.status(200).json({
+				message: 'User Deleted'
+			});
 		})
-	}).catch((err) => {
-		res.status(500).json({
-			err
-		})
->>>>>>> meti
-	});
+		.catch((err) => {
+			res.status(500).json({
+				err
+			});
+		});
 };
