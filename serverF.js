3c12b35a6dd02391d11fac5cb16726f654ff7583
Elsina <ebr.sina@outlook.com>
2020-02-02 11:43:23 +0000

    fromserver

--- serverF.js
+++ serverF.js
@@ -12,7 +12,7 @@ app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(
-    process.env.PORT || 3300,
+    process.env.PORT || 3000,
     function () {
         console.log(`Frontend start on http://localhost:3300`)
     }
